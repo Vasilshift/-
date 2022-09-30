b6208d5719a33266adea5565ac1432c79a44e464
vasilshift <evvbelgrad@gmail.com>
2022-09-30 16:14:25 +0200

    init commit

---
+++ src/main/java/com/stylefastbot/stylefastbot/StylefastbotApplication.java
@@ -0,0 +1,13 @@
+package com.stylefastbot.stylefastbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylefastbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StylefastbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stylefastbot/stylefastbot/StylefastbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stylefastbot.stylefastbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylefastbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
